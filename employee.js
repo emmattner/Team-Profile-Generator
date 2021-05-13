5fb5b422d259015940581bcdc492040942231136
Emma Mattner <emattner1997@gmail.com>
2021-05-13 21:58:46 +0930

    updating js and add package json

--- employee.js
+++ employee.js
@@ -1,36 +1,51 @@
-const Choices = require("inquirer/lib/objects/choices");
-const { prependOnceListener } = require("process");
+// const Choices = require("inquirer/lib/objects/choices");
+// const { prependOnceListener } = require("process");
 
+// const manager = 'MANAGER',
+// const ENGINEER = 'ENGINEER',
+// const INTERN = 'INTERN',
 
-prompt({
-    type : 'list',
-    message: 'Who do you want to create?',
-    choices: [
-        {name: 'Manager', value: 'MANAGER'},
-        {name: 'Engineer', value: 'Engineer'},
-        {name: 'Intern', value: 'INTERN'},
+// prompt ({
+//     type : 'list',
+//     message: 'Who do you want to create?',
+//     choices: [
+//         {name: 'Manager', value: 'MANAGER'},
+//         {name: 'Engineer', value: 'ENGINEER'},
+//         {name: 'Intern', value: 'INTERN'},
     
-    ]})
+//     ]})
 
-const promptUserQuestions = ({ usertype }) => prompt([
-    {
-        name: 'name',
-        type: 'input',
-        message: 'What is the persons name?'
-    },
-    {
-        name: 'skills',
-        type: 'list',
-        message: 'What are your skills?',
-        choices: ['CSS', 'HTML', 'JS'],
-        when: (answers) =>
-    },
-])
+// const promptUserQuestions = ({ usertype }) => prompt([
+//     {
+//         name: 'name',
+//         type: 'input',
+//         message: 'What is the persons name?'
+//     },
+//     {
+//         name: 'skills',
+//         type: 'list',
+//         message: 'What are your skills?',
+//         choices: ['CSS', 'HTML', 'JS'],
+//         when: (answers) =>
+//     },
+//     {
+//         name: 'email',
+//         type: 'input',
+//         message: 'What is your email?',
 
-con
+//     },
+//     {
+//         name: 'email',
+//         type: 'input',
+//         message: 'What is your email?',
 
-promptType().then((answers) => {
-    if (answer.userType === MANAGER) {
-        prependOnceListener.push( new MANAGER(answers.name, answers.email))
-    }
-})
+//     },
+// ])
+
+// con
+
+// promptType().then((answers) => {
+//     if (answer.userType === MANAGER) {
+//         prependOnceListener.push( new MANAGER(answers.name, answers.email))
+//     }
+// })
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const inquirer = require('inquirer')
 const manager = require('./manager')
 const engineer = require('./engineer')
 const intern = require('./intern')
+const { type } = require('os')
 
 const teamArray = [ ];
 
@@ -40,4 +41,68 @@ const addManager = () => {
         console.log(manager);
     })
 };
+
+const addEmployee = () => {
+    console.log('Adding employees');
+
+    return inquirer.prompt ([
+        {
+            name: 'role',
+            type: 'list',
+            message: ['engineer', 'intern']
+        },
+        {
+            name: 'name',
+            type: 'input',
+            message: "What's the name of the employee?", 
+        },
+        {
+            name: 'email',
+            type: 'input',
+            message: "What's your email?", 
+        },
+        {
+            name: 'officeNumber',
+            type: 'input',
+            message: "What's your office number?", 
+        },
+        {
+            name: 'github',
+            type: 'input',
+            message: "What is your Github?", 
+        },
+        {
+            name: 'officeNumber',
+            type: 'input',
+            message: "What's your office number?", 
+        },
+        {
+            name: 'school',
+            type: 'input',
+            message: "What school did you go to?", 
+        },
+    ])
+}
+
+then(employeeData => { 
+    let { name, id, email, role, github, school, confirmAddEmployees } = employeeData;
+    let employee;
+
+    if(role === 'engineer') {
+        employee = new Engineer (name, id, email, github);
+        console.log(employee);
+    }
+    else if (role === 'intern') {
+        employee = new Intern (name, id, email, school);
+        console.log(employee);
+    };
+    
+    teamArray.push(employee);
+
+    if (confirmAddEmployees) {
+        return addEmployee(teamArray);
+    } else {
+        return teamArray;
+    }
+})
 
