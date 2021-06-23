a0198835ffa53de05c8b2f40deeb3c2a6a5a191a
Emma Mattner <emattner1997@gmail.com>
2021-06-23 20:33:30 +0930

    creating employee object

--- tests/employee.tests.js
+++ tests/employee.tests.js
@@ -2,5 +2,8 @@ const Employee = require('../lib/Employee');
 
 test('creates an employee object', () => {
     const employee = new Employee('Emma', 90, 'emma@emma.com');
-    
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
 });
