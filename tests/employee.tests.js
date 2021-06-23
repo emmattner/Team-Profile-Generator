92452ca4530ac77039c3dadba4a3ddc3e93f4722
Emma Mattner <emattner1997@gmail.com>
2021-06-23 20:42:45 +0930

    getting ID

--- tests/employee.tests.js
+++ tests/employee.tests.js
@@ -1,9 +1,16 @@
 const Employee = require('../lib/Employee');
 
 test('creates an employee object', () => {
-    const employee = new Employee('Emma', 90, 'emma@emma.com');
+    const employee = new Employee('Emma', 82, 'emma@emma.com');
 
     expect(employee.name).toEqual(expect.any(String));
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
 });
+
+test('gets employee name', () => {
+    const employee = new Employee('Emma', 82, 'emma@emma.com')
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+
