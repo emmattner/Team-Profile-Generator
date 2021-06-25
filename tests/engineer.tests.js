d0eaf728682930d1866136e7333d98eaae3316fd
Emma Mattner <emattner1997@gmail.com>
2021-06-25 14:44:15 +0930

    getting role and github

--- tests/engineer.tests.js
+++ tests/engineer.tests.js
@@ -1,10 +1,23 @@
 const { TestWatcher } = require('jest');
 const { hasUncaughtExceptionCaptureCallback } = require('process');
 const { inspect } = require('util');
+const employee = require('../lib/employee');
 const Engineer = require('../lib/engineer');
 
 test('creates and Engineer object', () => {
     const engineer = new Engineer('Emma', 82, 'emma@emma.com', 'emmattner')
 
-    inspect(engineer.github) .toEqual(expect.any(String));
+    expect(engineer.github) .toEqual(expect.any(String));
+});
+
+test ('gets engineer github value', () => {
+    const engineer = new Engineer('Emma', 82, 'emma@emma.com', 'emmattner')
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
+
+test('gets role of employee', () => {
+    const engineer = new Engineer('Emma', 82, 'emma@emma.com', 'emmattner')
+
+    expect(engineer.getRole()).toEqual("Engineer");
 });
