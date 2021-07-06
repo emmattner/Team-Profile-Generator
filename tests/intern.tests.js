aa1b5e693af16b74a4b1eb42e6b64569552c3b31
Emma Mattner <emattner1997@gmail.com>
2021-07-06 19:08:16 +0930

    creating intern object

---
+++ tests/intern.tests.js
@@ -0,0 +1,8 @@
+const Intern = require('../lib/Intern');
+
+// creating intern object  
+test('creates an Intern object', () => {
+    const intern = new Intern('Nicole', 90, 'nicole.elisaw@gmail', 'SFSU');
+    
+    expect(intern.school) .toEqual(expect.any(String));
+});
