de0c283eba3e3bf165ffe3ce5a472c09135d4d09
Emma Mattner <emattner1997@gmail.com>
2021-07-06 19:03:51 +0930

    writing employye tests

--- tests/employee.tests.js
+++ tests/employee.tests.js
@@ -10,23 +10,23 @@ test('creates an employee object', () => {
 
 test('gets employee name', () => {
     const employee = new Employee('Emma', 82, 'emma@emma.com')
-    expect(employee.getName()).toEqual(expect.any(String));
+    expect(employee.getName()).toEqual(expect.stringContaining('Emma'));
 });
 
 test('gets employee ID', () => {
     const employee = new Employee('Emma', 82, 'emma@emma.com')
-    expect(employee.getId()).toEqual(expect.any(Number));
+    expect(employee.getId()).toEqual(expect.stringContaining(82));
 });
 
 test('gets employee email', () => {
     const employee = new Employee('Emma', 82, 'emma@emma.com')
     
-    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+    expect(employee.getEmail()).toEqual(expect.stringContaining('emma@emma.com'));
 });
 
 test('gets role of employee', () => {
     const employee = new Employee('Emma', 82, 'emma@emma.com');
     
-    expect(employee.getRole()).toEqual("Employee");
+    expect(employee.getRole()).toEqual(expect.stringContaining('Employee'));
 }); 
 
