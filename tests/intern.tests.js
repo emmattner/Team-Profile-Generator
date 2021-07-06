c149901829695c8d1e779b70c8774a188e013ed3
Emma Mattner <emattner1997@gmail.com>
2021-07-06 20:41:45 +0930

    updating intern test

--- tests/intern.tests.js
+++ tests/intern.tests.js
@@ -6,3 +6,10 @@ test('creates an Intern object', () => {
     
     expect(intern.school) .toEqual(expect.any(String));
 });
+
+test('gets employee school', () => {
+    const intern = new Intern('Nicole', 90, 'nicole.elisaw@gmail', 'SFSU');
+    
+    expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
+});
+
