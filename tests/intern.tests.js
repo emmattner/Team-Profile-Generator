f14a269620cc4ed6f6c4ad588f9a26c6c1d381ca
Emma Mattner <emattner1997@gmail.com>
2021-07-06 20:43:15 +0930

    updating intern test

--- tests/intern.tests.js
+++ tests/intern.tests.js
@@ -2,13 +2,13 @@ const Intern = require('../lib/Intern');
 
 // creating intern object  
 test('creates an Intern object', () => {
-    const intern = new Intern('Nicole', 90, 'nicole.elisaw@gmail', 'SFSU');
+    const intern = new Intern('Nicole', 82, 'emma.emma@gmail', 'PPS');
     
     expect(intern.school) .toEqual(expect.any(String));
 });
 
 test('gets employee school', () => {
-    const intern = new Intern('Nicole', 90, 'nicole.elisaw@gmail', 'SFSU');
+    const intern = new Intern('Nicole', 82, 'emma.emma@gmail', 'PPS');
     
     expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
 });
