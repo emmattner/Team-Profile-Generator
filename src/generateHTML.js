6a211ac733a45ba4be603d38b9654211e187b3ae
Emma Mattner <emattner1997@gmail.com>
2021-07-12 16:49:38 +0930

    updated generatehtml js

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -8,7 +8,7 @@ const generateManager = function (manager) {
     </div>
             
     <div class="card-body">
-        <p class="id">ID:</p>
+        <p class="id">ID:${manager.id}</p>
         <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
         <p class="office">Office Number: ${manager.officeNumber}</p>
         </div>
